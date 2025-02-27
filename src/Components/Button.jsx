81747cf27871b79c00b2da2ffe697be02e093153
rahulgurudu-2003 <gurudurahul17@.gmail.com>
2025-02-27 19:46:40 +0530

    updated button and recommended components

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,3 +1,13 @@
+// const Button = ({ onClickHandler, value, title }) => {
+//   return (
+//     <button onClick={onClickHandler} value={value} className="btns">
+//       {title}
+//     </button>
+//   );
+// };
+
+// export default Button;
+
 const Button = ({ onClickHandler, value, title }) => {
   return (
     <button onClick={onClickHandler} value={value} className="btns">
